8ebc69564109e1181fae93b29dc62ccbc9454b7f
Hoang Hy <hoanghy01122003@gmail.com>
2024-09-01 02:40:07 +0700

    feat: add feature to add multiple lecturer

--- src/components/AddLecturerUser.tsx
+++ src/components/AddLecturerUser.tsx
@@ -32,6 +32,7 @@ import toast from "react-hot-toast";
 import FacultySelector from "./selectors/FacultySelector";
 import { FilterProvider, useFilter } from "@/contexts/FilterContext";
 import LecturerSelector from "./LecturerSelector";
+import { v4 as uuidv4 } from "uuid";
 
 const columns = [
 	{
@@ -49,6 +50,7 @@ type Props = {
 };
 
 export default function AddLecturerUser() {
+	const [selectedKeys, setSelectedKeys] = useState(new Set([]));
 	const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
 	const { data } = useAllLecturersQuery({
@@ -58,30 +60,42 @@ export default function AddLecturerUser() {
 	const [mutate] = useRegisterUserMutation();
 
 	const handleAddUser = useCallback(
-		async (lecturer: Lecturer) => {
+		async (lecturer: Partial<Lecturer>) => {
 			try {
 				if (!lecturer.email) return;
-				const password = "";
-				const promise = mutate({
+				await mutate({
 					variables: {
 						user: {
 							displayName: lecturer.display_name,
 							username: lecturer.email,
 							lecturerId: lecturer?.lecturer_id,
 							role: Role.Lecturer,
-							password,
+							password: uuidv4(),
 						},
 					},
-				});
-				await toast.promise(promise, {
-					loading: "Đang tạo tài khoản...",
-					success: "Tạo tài khoản thành công",
-					error: "Tên đăng nhập đã tồn tại",
 				});
 			} catch (error) {}
 		},
 		[mutate]
 	);
+
+	const handleAddMultipleUser = useCallback(async () => {
+		const promise = Promise.all(
+			Array.from(selectedKeys.values()).map(async (id) => {
+				const lecturer = data?.lecturers.data.find(
+					(value) => value.lecturer_id === id
+				);
+				console.log({ lecturer });
+				if (lecturer) await handleAddUser(lecturer);
+				return id;
+			})
+		);
+		await toast.promise(promise, {
+			loading: "Đang tạo tài khoản...",
+			success: "Tạo tài khoản thành công",
+			error: "Lỗi tạo tài khoản",
+		});
+	}, [data?.lecturers.data, handleAddUser, selectedKeys]);
 
 	return (
 		<div>
@@ -91,39 +105,66 @@ export default function AddLecturerUser() {
 			<Modal isOpen={isOpen} onOpenChange={onOpenChange}>
 				<ModalContent>
 					{(onClose) => (
-						<div>
-							<Table
-								aria-label="Selection behavior table example with dynamic content"
-								selectionMode="multiple"
-								selectionBehavior={"toggle"}
-							>
-								<TableHeader columns={columns}>
-									{(column) => (
-										<TableColumn key={column.key}>
-											{column.label}
-										</TableColumn>
-									)}
-								</TableHeader>
-								<TableBody
-									items={(data?.lecturers.data || []).map(
-										(value, index) => ({
-											index,
-											display_name: value.display_name,
-										})
-									)}
-								>
-									{(item) => (
-										<TableRow key={item.display_name}>
-											{(columnKey) => (
-												<TableCell>
-													{getKeyValue(item, columnKey)}
-												</TableCell>
+						<>
+							<ModalBody>
+								<div className=" max-h-[600px] overflow-auto">
+									<Table
+										aria-label="Lecturer table"
+										selectionMode="multiple"
+										selectionBehavior={"toggle"}
+										selectedKeys={selectedKeys}
+										onSelectionChange={setSelectedKeys}
+									>
+										<TableHeader columns={columns}>
+											{(column) => (
+												<TableColumn key={column.key}>
+													{column.label}
+												</TableColumn>
 											)}
-										</TableRow>
-									)}
-								</TableBody>
-							</Table>
-						</div>
+										</TableHeader>
+										<TableBody
+											items={(data?.lecturers.data || []).map(
+												(value, index) => ({
+													...value,
+													index,
+												})
+											)}
+										>
+											{(item: Lecturer) => (
+												<TableRow key={item.lecturer_id}>
+													{(columnKey) => (
+														<TableCell>
+															{getKeyValue(
+																item,
+																columnKey
+															)}
+														</TableCell>
+													)}
+												</TableRow>
+											)}
+										</TableBody>
+									</Table>
+								</div>
+							</ModalBody>
+							<ModalFooter>
+								<Button
+									color="danger"
+									variant="light"
+									onPress={onClose}
+								>
+									Đóng
+								</Button>
+								<Button
+									color="primary"
+									onPress={async () => {
+										await handleAddMultipleUser();
+										onClose();
+									}}
+								>
+									Tạo tài khoản
+								</Button>
+							</ModalFooter>
+						</>
 					)}
 				</ModalContent>
 			</Modal>
