2039a31431c70612395938c383251fbb43b6f41f
Hoang Hy <hoanghy01122003@gmail.com>
2025-05-25 20:21:59 +0700

    feat: add neutral comment

--- src/components/comments/CommentQuantityInfo.tsx
+++ src/components/comments/CommentQuantityInfo.tsx
@@ -5,6 +5,7 @@ import InfoTab from "@/components/InfoTab";
 import ALL_COMMENT_ICON from "@assets/all_comment.svg";
 import NEGATIVE_COMMENT_ICON from "@assets/negative_comment.svg";
 import POSITIVE_COMMENT_ICON from "@assets/positive_comment.svg";
+import NEUTRLA_COMMENT_ICON from "@assets/neutral_comment.svg";
 
 import { useCommentQuantityQuery } from "@/gql/graphql";
 import { useRememberValue } from "@/hooks/useRememberValue";
@@ -44,6 +45,13 @@ export default function CommentQuantityInfo({ query }: Props) {
 				title="Tiêu cực"
 				isLoading={isLoading}
 				number={data?.negative.quantity}
+			/>
+			<InfoTab
+				type="neutral"
+				icon={NEUTRLA_COMMENT_ICON}
+				title="Trung tính"
+				isLoading={isLoading}
+				number={0}
 			/>
 		</>
 	);
